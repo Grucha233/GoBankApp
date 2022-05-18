a49c9912370dd41b6d7b49909e6068c5c987a848
Grucha233 <adam.gr905@gmail.com>
2022-05-18 15:37:01 +0200

    Added proper JWT creation. Also implemented Authorization function, and alredy tested it - it works

--- cmd/bank/api.go
+++ cmd/bank/api.go
@@ -9,9 +9,10 @@ import (
 //
 // Main api function, handles all endpoints
 func HandleRequests() {
-	http.HandleFunc("/", HomePage)
+	http.Handle("/", IsAuthorized(HomePage))
 	http.HandleFunc("/register", Register)
 	http.HandleFunc("/login", Login)
+	http.HandleFunc("/check", Check)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
@@ -24,7 +25,7 @@ func Register(w http.ResponseWriter, r *http.Request) {
 
 	case "POST":
 		fmt.Fprintf(w, "Login page - POST")
-		UserRegister(r)
+		UserRegister(w, r)
 
 	default:
 		w.WriteHeader(http.StatusNotImplemented)
@@ -35,7 +36,21 @@ func Register(w http.ResponseWriter, r *http.Request) {
 //
 // Allows users to log in to the bank, "/login"
 func Login(w http.ResponseWriter, r *http.Request) {
-	UserLogin(r)
+	switch r.Method {
+	case "GET":
+		fmt.Println("Welcome to Login page, please pass credentials to LogIn")
+
+	case "POST":
+		fmt.Println("Trying to log in")
+		Signin(w, r)
+
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte(http.StatusText(http.StatusNotImplemented)))
+	}
+}
+func Check(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Checking function")
 }
 
 //
@@ -44,10 +59,3 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
 }
-
-// usrStr := `{
-// 	"Name":"adas",
-// 	"Balance": "170",
-// 	"Currency": "zloty",
-// 	"Password": "123"
-//  }`
--- cmd/bank/userFunc.go
+++ cmd/bank/userFunc.go
@@ -3,17 +3,21 @@ package bank
 import (
 	"fmt"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
 	"golang.org/x/crypto/bcrypt"
 )
 
-const SecretKey = "123"
+var jwtKey = []byte("123")
+
+type Claims struct {
+	Name string `json:"Name"`
+	jwt.StandardClaims
+}
 
 //This function inserts user data into database
-func UserRegister(r *http.Request) {
+func UserRegister(w http.ResponseWriter, r *http.Request) {
 	//Connect to the database
 	db, err1 := ConGorm()
 	if err1 != nil {
@@ -38,12 +42,13 @@ func UserRegister(r *http.Request) {
 }
 
 //This function auth user, and creates JWT for him(1h)
-func UserLogin(r *http.Request) (string, error) {
+func Signin(w http.ResponseWriter, r *http.Request) {
 	//Connect to the database
 	db, err1 := ConGorm()
 	if err1 != nil {
 		fmt.Println("Problem with gorm connect")
-		return "", err1
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	//Parsing POST request
 	err2 := r.ParseForm()
@@ -56,27 +61,82 @@ func UserLogin(r *http.Request) (string, error) {
 
 	if user.ID == 0 {
 		fmt.Println("Wrong credentials, please try again")
-		return "", err1
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 	//Checking if given password is correct
 	if err2 := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(r.Form.Get("Password"))); err2 != nil {
 		fmt.Println("Wrong credentials, please try again2")
-		return "", err2
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
 
-	fmt.Println("Succesfully login")
-	//Creating a JWT tocken
-	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
-		Issuer:    strconv.Itoa(int(user.ID)),
-		ExpiresAt: time.Now().Add(time.Hour * 1).Unix(),
-	})
+	////////////////////////////////Creating a JWT tocken////////////////////////////////
 
-	token, err := claims.SignedString([]byte(SecretKey))
+	//Declare expiration time
+	expirationTime := time.Now().Add(1 * time.Hour)
 
+	//Create JWT claims with given UserName and expiry time
+	claims := &Claims{
+		Name: user.Name,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	//Creation of JWT tocken based on claims(Username(Name in database) and epiry time)
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	//Converting token to String
+	tokenString, err := token.SignedString(jwtKey)
+	//If something happens then return error
 	if err != nil {
-		fmt.Println("Something Went Wrong")
-
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-	fmt.Println(token)
-	return token, nil
+	//Sets Cookie with JWT
+	http.SetCookie(w, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+	fmt.Println("Succesfully login")
+}
+
+//Function checks if JWT is valid and if yup then gives permission to other functionalities of app
+func IsAuthorized(endpoint func(http.ResponseWriter, *http.Request)) http.Handler {
+	//Return of the original endpoint if JWT is Valid
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//Geting JWT from Cookie
+		c, err := r.Cookie("token")
+		if err != nil {
+			if err == http.ErrNoCookie {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		//Geting JWT into variable
+		tokenString := c.Value
+
+		claims := &Claims{}
+
+		//Parse the JWT and store in claims object
+		token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+		if err != nil {
+			if err == jwt.ErrSignatureInvalid {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if token.Valid {
+			endpoint(w, r)
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+		}
+	})
 }
